cfd414bb5911d7dc33d966bfe0ff57d3e73d16d8
leeeunso <127708509+leeeunso@users.noreply.github.com>
2023-10-17 21:15:13 +0900

    4,5주차

---
+++ src/w4/W4/assignment/step2/script/Mover.js
@@ -0,0 +1,75 @@
+class Mover {
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.accDisplay = createVector(0, 0);
+    this.mass = mass;
+    this.radius = this.mass ** 0.5 * 10;
+  }
+  applyForce(force) {
+    let forceDividedByMass = createVector(force.x, force.y);
+    forceDividedByMass.div(this.mass);
+    this.acc.add(forceDividedByMass);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.accDisplay.set(this.acc);
+    this.acc.mult(0);
+  }
+
+  contactEdges() {
+    if (this.pos.y >= height - 1 - this.radius - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkEdges() {
+    const bounce = -0.5;
+    if (this.pos.x < 0 + this.mass) {
+      this.pos.x -= 0 + this.mass;
+      this.pos.x *= -1;
+      this.pos.x += 0 + this.mass;
+      this.vel.x *= -1;
+    } else if (this.pos.x > width - 1 - this.radius) {
+      this.pos.x -= width - 1 - this.radius;
+      this.pos.x *= -1;
+      this.pos.x += width - 1 - this.radius;
+      this.vel.x *= bounce;
+    }
+    if (this.pos.y > height - 1 - this.radius) {
+      this.pos.y -= height - 1 - this.radius;
+      this.pos.y *= -1;
+      this.pos.y += height - 1 - this.radius;
+      this.vel.y *= bounce;
+    }
+    // 0보다 뚫고 간 만큼을 0에 더해주면 정확하게 튕긴 위치가 됨
+  }
+
+  display() {
+    noStroke();
+    fill(0);
+    ellipse(this.pos.x, this.pos.y, 5 * this.radius);
+  }
+
+  displayVector() {
+    stroke('red');
+    line(
+      this.posx,
+      this.pos.y,
+      this.pos.x + this.vel.x * 10,
+      this.pos.y + this.vel.y * 10
+    );
+    stroke('blue');
+    line(
+      this.posx,
+      this.pos.y,
+      this.pos.x + this.accDisplay.x * 100,
+      this.pos.y + this.accDisplay.y * 100
+    );
+  }
+}
---
+++ src/w4/W4/assignment/step2/script/sketch.js
@@ -0,0 +1,45 @@
+let moverA;
+let moverB;
+let wind;
+let gravity;
+
+function setup() {
+  setCanvasContainer('canvas', 3, 2, true);
+  background(255);
+  moverA = new Mover(width / 3, height / 2, 10);
+  moverB = new Mover((2 * width) / 3, height / 2, 1);
+  gravity = createVector(0, 0.1);
+  wind = createVector(0.2, 0);
+}
+
+function draw() {
+  background(255);
+
+  let gravityA = createVector(gravity.x, gravity.y);
+  gravityA.mult(moverA.mass);
+  moverA.applyForce(gravityA);
+  if (mouseIsPressed && isMouseInsideCanvas()) {
+    // 마우스 클릭할때만 바람 불게 만들기
+    moverA.applyForce(wind);
+  }
+  if (moverA.contactEdges()) {
+    let c = 0.9;
+    // let friction = createVector(moverA.vel.x, moverA.vel.y);
+    let friction = moverA.vel.copy();
+    friction.mult(-1);
+    friction.mult(c);
+    moverA.applyForce(friction);
+  }
+  moverA.update();
+  moverA.checkEdges();
+  moverA.display();
+  moverA.displayVector();
+
+  let gravityB = createVector(gravity.x, gravity.y);
+  gravityB.mult(moverB.mass);
+  moverB.applyForce(gravityB);
+  if (mouseIsPressed && isMouseInsideCanvas()) {
+    // 마우스 클릭할때만 바람 불게 만들기
+    moverB.applyForce(wind);
+  }
+}
