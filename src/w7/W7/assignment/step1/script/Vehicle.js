db1a444f8bfc45f714a5ac2c16b727e121d682a3
leeeunso <127708509+leeeunso@users.noreply.github.com>
2023-11-06 20:50:31 +0900

    커밋

--- src/w7/W7/assignment/step1/script/Vehicle.js
+++ src/w7/W7/assignment/step1/script/Vehicle.js
@@ -23,79 +23,114 @@ class Vehicle {
   }
 
   cohesion(others) {
+    //다른 것들을 불러옴
     let cnt = 0;
+    //cnt 변수 지정
     const steer = createVector(0, 0);
+    //더해졋다가 나누게 되는것이 steer가 됨
     others.forEach((each) => {
       if (each !== this) {
+        //만약에 each가 나랑 똑같지 않을때만
         const distSq =
           (this.pos.x - each.pos.x) ** 2 + (this.pos.y - each.pos.y) ** 2;
+        //우리의 위치의 x값에서 each의 x값을 뺌, 그것을 제곱, (y에 대해서도 똑같이)
         if (distSq < this.neighborhooodRad ** 2) {
+          //만약에 if distSq가 나의 neighborhooodRad의 제곱보다 작을때에만 나머지와 계산
           steer.add(each.pos);
+          //steer에게 each.pos(위치)을 더한다.
           cnt++;
+          //계산이 일어날때마다 cnt가 더해짐
         }
       }
     });
     if (cnt > 0) {
+      //cnt가 0이 아니면
       steer.div(cnt);
-      steer.sub(this.pos);
+      //만약 cnt가 0이 아니면 steer를 div해줌
       steer.setMag(this.speedMx);
+      //steer를 this.speedMx로 setMag 함 (벡터의 크기를 매개변수 len의 값으로 제한)
       steer.sub(this.vel);
+      //steer를 his.vel로 sub함 특정 키에 현재 저장된 값에서 지정한 숫자를 빼고, 그 결과값은 사전 안에 저장되어있던 기존값을 대체
       steer.limit(this.forceMx);
       //실제로 적용할 수 있는 힘의 최대치
     }
     return steer;
+    // 반환할 값을 지정
   }
 
   align(others) {
+    //다른 것들을 불러옴
     let cnt = 0;
+    //cnt 변수 지정
     const steer = createVector(0, 0);
     others.forEach((each) => {
       if (each !== this) {
+        //each가 내가 아닐때만 계산을 수행
         const distSq =
+          //거리 계산 (거리를 정확한 값으로 알 필요X)
           (this.pos.x - each.pos.x) ** 2 + (this.pos.y - each.pos.y) ** 2;
+        //우리의 위치의 x값에서 each의 x값을 뺌, 그것을 제곱, (y에 대해서도 똑같이)
         if (distSq < this.neighborhooodRad ** 2) {
+          //만약에 if distSq가 우리의 neighborhooodRad의 제곱보다 작으면 그때는 우리의 친구에 해당함
           steer.add(each.vel);
           //   steer.add(p5.Vector.normalize(each.vel));
+          //steer에게 each.vel(속도)을 더한다.
           cnt++;
+          //계산이 일어날때마다 cnt가 더해짐
         }
       }
     });
     if (cnt > 0) {
+      //cnt가 0이 아니면
       steer.div(cnt);
+      //만약 cnt가 0이 아니면 steer를 div해줌
       steer.setMag(this.speedMx);
+      //steer를 this.speedMx로 setMag 함 (벡터의 크기를 매개변수 len의 값으로 제한)
       steer.sub(this.vel);
+      //steer를 his.vel로 sub함 특정 키에 현재 저장된 값에서 지정한 숫자를 빼고, 그 결과값은 사전 안에 저장되어있던 기존값을 대체
       steer.limit(this.forceMx);
+      //실제로 적용할 수 있는 힘의 최대치
     }
     return steer;
+    //반환할 값을 지정
   }
 
   separate(others) {
     //separate로 계산을 하려면 다른 정보가 있어야하기 때문에 others 입력
     let cnt = 0;
-    //
+    //계산이 일어날때마다 cnt가 더해짐
     const steer = createVector(0, 0);
     //각각 충돌하는 벡터들을 일정 계샇나여 더해 하나의 벡터로 만들기 위한 공간
     others.forEach((each) => {
       if (each !== this) {
         const dist = this.pos.dist(each.pos);
         if (dist > 0 && this.rad + each.rad > dist) {
+          //dist가 0보다 크다. (dist가 0으로 나눠지는 것을 방지)
           const distNormal = dist / (this.rad + each.rad);
           const towardMeVec = p5.Vector.sub(this.pos, each.pos);
+          //나한테 오는 벡터는 p5.Vector.sub해서 나의 위치에서 each.pos 해서 towardMeVec를 구할 수 있음
           towardMeVec.setMag(1 / distNormal);
+          //towardMeVec를 1 / distNormal로 길이를 줄임 (얼마나 깊숙이 들어왔는지)
           steer.add(towardMeVec);
           //steer에게 towardMeVec을 더한다.
           cnt++;
+          //계산이 일어날때마다 cnt가 더해짐
         }
       }
     });
     if (cnt > 0) {
+      //만약 cnt가 0이 아니면 steer를 div해줌
       steer.div(cnt);
+      //cnt로 div
       steer.setMag(this.speedMx);
+      //steer를 this.speedMx로 setMag 함 (벡터의 크기를 매개변수 len의 값으로 제한)
       steer.sub(this.vel);
+      //steer를 his.vel로 sub함 특정 키에 현재 저장된 값에서 지정한 숫자를 빼고, 그 결과값은 사전 안에 저장되어있던 기존값을 대체
       steer.limit(this.forceMx);
       //실제로 적용할 수 있는 힘의 최대치
     }
     return steer;
+    //반환할 값을 지정
   }
 
   applyForce(force) {
@@ -122,14 +157,18 @@ class Vehicle {
     // 객체가 InfiniteOffset의 범위를 벗어나면 다시 어디서 나타나야 하는지를 설명하는 함수
     if (this.pos.x < -infiniteOffset) {
       this.pos.x = width + infiniteOffset;
+      //만약 this.pos.x < -infiniteOffset 라면 this.pos.x는 width + infiniteOffset이고
     } else if (this.pos.x > width + infiniteOffset) {
+      //this.pos.x가 width + infiniteOffset보다 크다면
       this.pos.x = -infiniteOffset;
+      //this.pos.x 는 -infiniteOffset이다
     }
     if (this.pos.y < -infiniteOffset) {
       this.pos.y = height + infiniteOffset;
     } else if (this.pos.y > height + infiniteOffset) {
       this.pos.y = -infiniteOffset;
     }
+    // y 와 height였을때도 동일하게 적용
   }
 
   display() {
